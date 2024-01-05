35311f86e2672a2e6382e55609b44eedf0614760
cheruiyotrop <cheruiyot.rop@student.moringaschool.com>
2024-01-05 21:04:52 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// index.js
+const divide = function(x, y) {
+    return 2000 / 100;
+
+};
+
+module.exports =divide;
+
+const square = function(x, y){
+    return square;
+    module.exports= square;
+}
